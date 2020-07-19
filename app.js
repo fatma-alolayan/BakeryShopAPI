49eb03e98c66be23ebdda0d52f1b531a090e4eb0
fatma-alolayan <temaalolayan@hotmail.com>
2020-07-19 19:24:10 +0300

    express

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const items = require("./items");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.listen(8000, () => {
+  console.log("The application is running on localhost:8000");
+});
+
+app.get("/", (req, res) => {
+  console.log("HELLO");
+  res.json({ message: "Hello World" });
+});
+
+app.get("/items", (req, res) => {
+  res.json(items);
+});
