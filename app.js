d804fff67c3621a0bf8ea164745d542851e43b56
fatma-alolayan <temaalolayan@hotmail.com>
2020-07-20 17:50:17 +0300

    delete

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const items = require("./items");
+let items = require("./items");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
-
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
 
 app.get("/", (req, res) => {
   console.log("HELLO");
@@ -16,4 +12,19 @@ app.get("/", (req, res) => {
 
 app.get("/items", (req, res) => {
   res.json(items);
+});
+
+app.delete("/items/:itemsId", async (req, res) => {
+  const { itemsId } = req.params;
+  const foundItem = items.find((item) => item.id === +itemsId);
+  if (foundItem) {
+    items = items.filter((_items) => _items.id !== +itemsId);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "item not found" });
+  }
+});
+
+app.listen(8000, () => {
+  console.log("The application is running on localhost:8000");
 });
